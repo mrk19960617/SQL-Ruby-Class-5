d6cced0b29a6e72f2948cb15f65fbe5cf7ed39c8
mrk19960617 Lienhard <mark.lienhard@kellogg.northwestern.edu>
2025-02-04 20:21:42 +0000

    Changes

---
+++ app/models/company.rb
@@ -0,0 +1,3 @@
+class Company < ApplicationRecord
+    #Responsible for connecting with the company file
+end
--- db/migrate/20250204201659_create_contacts.rb
+++ db/migrate/20250204201659_create_contacts.rb
@@ -1,9 +1,10 @@
-class CreateSalespeople < ActiveRecord::Migration[7.1]
+class CreateContacts < ActiveRecord::Migration[7.1]
   def change
-    create_table :salespeople do |t|
+    create_table :contacts do |t|
       t.string "first_name"
       t.string "last_name"
       t.string "email"
+      t.integer "company_id"
 
       t.timestamps
     end
